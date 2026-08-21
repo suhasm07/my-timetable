c538f443e61942501a6a82fafed5bf87f3d428ca
suhasm07 <suhasm651@gmail.com>
2026-08-21 16:28:11 +0530

    Add Student class to track enrolled courses

---
+++ src/Student.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// Represents the student using the program and the courses they've enrolled into.
+public class Student {
+
+    // The list of courses the student is currently enrolled in.
+    private List<Course> coursesEnrolled;
+
+    public Student() {
+        this.coursesEnrolled = new ArrayList<>();
+    }
+
+    // Checks if a student is enrolled in the course or not
+    public boolean isEnrolled(Course course) {
+        return coursesEnrolled.contains(course);
+    }
+
+    // Adds a course to the student's enrolled list.
+    public void enroll(Course course) {
+        coursesEnrolled.add(course);
+    }
+
+    // Returns a copy of the courses enrolled
+    public List<Course> getCoursesEnrolled() {
+        List<Course> copy = new ArrayList<>(coursesEnrolled);
+        return copy;
+    }
+}
